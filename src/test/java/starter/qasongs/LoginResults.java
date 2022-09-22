c1b416cd1dda3feddc914a80445269695ca46159
Jakub Polkowski <y0d7@nykredit.dk>
2022-09-22 16:41:57 +0200

    my own test data

--- src/test/java/starter/qasongs/LoginResults.java
+++ src/test/java/starter/qasongs/LoginResults.java
@@ -4,6 +4,6 @@ import net.serenitybdd.core.pages.PageComponent;
 
 public class LoginResults extends PageComponent {
     public String errorMsg() {
-        return $("p.small").getText();
+        return find(".small").getText();
     }
 }
